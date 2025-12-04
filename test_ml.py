a07ab33982b95c0bd57313383d7c8899226ba298
Raj Rana <rrana7@wgu.edu>
2025-12-03 20:02:17 -0500

    Update unit tests and add passing CI screenshot

--- test_ml.py
+++ test_ml.py
@@ -1,13 +1,18 @@
 import pytest
 # TODO: add necessary import
+import numpy as np
+from ml.model import train_model, compute_model_metrics
+
 
 # TODO: implement the first test. Change the function name and input as needed
 def test_one():
     """
     # add description for the first test
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 1, 1, 0])
+    model = train_model(X, y)
+    assert model is not None
 
 
 # TODO: implement the second test. Change the function name and input as needed
@@ -15,8 +20,12 @@ def test_two():
     """
     # add description for the second test
     """
-    # Your code here
-    pass
+    y = np.array([0, 1, 1, 0])
+    preds = np.array([0, 1, 0, 0])
+    precision, recall, fbeta = compute_model_metrics(y, preds)
+    assert isinstance(precision, float)
+    assert isinstance(recall, float)
+    assert isinstance(fbeta, float)
 
 
 # TODO: implement the third test. Change the function name and input as needed
@@ -24,5 +33,8 @@ def test_three():
     """
     # add description for the third test
     """
-    # Your code here
-    pass
+    X = np.array([[0, 1], [1, 0], [1, 1], [0, 0]])
+    y = np.array([0, 1, 1, 0])
+    model = train_model(X, y)
+    preds = model.predict(X)
+    assert len(preds) == len(y)
